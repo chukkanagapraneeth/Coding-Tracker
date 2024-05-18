6badf68a097011c856f2eec6353dc28db4c58b15
chukkanagapraneeth <cnagapraneeth@gmail.com>
2024-05-18 22:47:07 +0530

    1) Set the working directory 2) Created app.config XML file for connectionString 3) Added code to create Sqlite db and table 4) executed the code and created coding-tracker.db file.

--- Coding-Tracker/Program.cs
+++ Coding-Tracker/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Configuration;
+using Microsoft.Data.Sqlite;
+using Spectre;
+
+namespace Coding_Tracker;
+
+class Program
+{
+    static string connectionstring = ConfigurationManager.AppSettings.Get("connectionString");
+
+    static void Main()
+    {
+        using(var connection = new SqliteConnection(connectionstring))
+        {
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS coding_tracker (Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Duration TEXT)";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
